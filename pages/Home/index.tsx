7e2d496e27924e422c411879ce0c9974d920a339
SongYoungNae <canb38@naver.com>
2022-12-12 12:10:44 +0900

    221212

--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -31,6 +31,9 @@ export default function Home() {
         md: true,
     });
     const [isSlideOpen, setSlideOpen] = useState(false);
+    const color1 = "#ffffff";
+    const color2 = "#000000";
+
     return (
         <>
             <Box w="100%" display={isSlideOpen ? "none" : "flex"}>
@@ -38,7 +41,7 @@ export default function Home() {
                     <VStack w="100%" h={500}>
                         <Image position="absolute" zIndex="-1" src={"/images/banner_test_2.jpeg"} alt="마루미디어" w="100%" h={500} />
 
-                        <Box w="100%" alignItems="center" mb="4" px="8" py="4">
+                        <Box w="100%" alignItems="center" mb="4" px="8" py="4" style={{ position: "fixed" }}>
                             <HStack w="100%" maxW="1100" justifyContent="space-between">
                                 <Hidden till="md">
                                     <>
@@ -107,6 +110,84 @@ export default function Home() {
                             </Center>
                         </Card>
                     </Box>
+                    <Center mt={50} w="100%">
+                        <Image position="absolute" src={"/images/banner_test_3.jpeg"} alt="마루미디어" w="100%" h={500}></Image>
+                        <Box
+                            w="100%"
+                            h={500}
+                            bg={{
+                                linearGradient: {
+                                    colors: ["#00000000", "cyan.900"],
+                                    start: [0, 0],
+                                    end: [1, 0],
+                                },
+                            }}
+                        >
+                            <Center py={3} _text={{ fontSize: "lg", textAlign: "center" }} color="coolGray.800" fontWeight="extrabold">
+                                미디어 시대를 살아가는 요즘 당신의 삶은 콘텐츠로 잘 표현되고 있나요? 잠시도 우리 곁을 떠나지 않는 다양한 영상들
+                                그런데! 정작 나에게 필요한 영상들 지금 딱 마음에 드는 영상들은 주변에서 찾기 어려우시죠? 마루미디어는 20년 동안
+                                방송가에 살아온 이들로 이루어진 콘텐츠 제작 업체로, 간단한 촬영에서부터 다양한 영상제작은 물론이고, 사람들이 원하는
+                                홍보영상까지 만들어 드리고 있습니다.{" "}
+                            </Center>
+                        </Box>
+                    </Center>{" "}
+                    <Box mt="16" flexWrap="wrap" width={{ base: "80%", sm: "50%", md: "50%" }} justifyContent={{ base: "", sm: "center" }}>
+                        <Card>
+                            <Center py={3} _text={{ fontSize: "lg", textAlign: "center" }} color="coolGray.800" fontWeight="extrabold">
+                                미디어 시대를 살아가는 요즘 당신의 삶은 콘텐츠로 잘 표현되고 있나요? 잠시도 우리 곁을 떠나지 않는 다양한 영상들
+                                그런데! 정작 나에게 필요한 영상들 지금 딱 마음에 드는 영상들은 주변에서 찾기 어려우시죠? 마루미디어는 20년 동안
+                                방송가에 살아온 이들로 이루어진 콘텐츠 제작 업체로, 간단한 촬영에서부터 다양한 영상제작은 물론이고, 사람들이 원하는
+                                홍보영상까지 만들어 드리고 있습니다.{" "}
+                            </Center>
+                        </Card>
+                    </Box>
+                    <Center mt={50} w="100%">
+                        <Image position="absolute" src={"/images/banner_test_4.jpeg"} alt="마루미디어" w="100%" h={500}></Image>
+                        <Box
+                            w="100%"
+                            h={500}
+                            bg={{
+                                linearGradient: {
+                                    colors: ["#00000000", "blue.800"],
+                                    start: [0, 0],
+                                    end: [1, 0],
+                                },
+                            }}
+                        >
+                            <Center py={3} _text={{ fontSize: "lg", textAlign: "center" }} color="coolGray.800" fontWeight="extrabold">
+                                미디어 시대를 살아가는 요즘 당신의 삶은 콘텐츠로 잘 표현되고 있나요? 잠시도 우리 곁을 떠나지 않는 다양한 영상들
+                                그런데! 정작 나에게 필요한 영상들 지금 딱 마음에 드는 영상들은 주변에서 찾기 어려우시죠? 마루미디어는 20년 동안
+                                방송가에 살아온 이들로 이루어진 콘텐츠 제작 업체로, 간단한 촬영에서부터 다양한 영상제작은 물론이고, 사람들이 원하는
+                                홍보영상까지 만들어 드리고 있습니다.{" "}
+                            </Center>
+                        </Box>
+                    </Center>{" "}
+                    <Box mt="16" flexWrap="wrap" width={{ base: "80%", sm: "50%", md: "50%" }} justifyContent={{ base: "", sm: "center" }}>
+                        <Card>
+                            <Center py={3} _text={{ fontSize: "lg", textAlign: "center" }} color="coolGray.800" fontWeight="extrabold">
+                                미디어 시대를 살아가는 요즘 당신의 삶은 콘텐츠로 잘 표현되고 있나요? 잠시도 우리 곁을 떠나지 않는 다양한 영상들
+                                그런데! 정작 나에게 필요한 영상들 지금 딱 마음에 드는 영상들은 주변에서 찾기 어려우시죠? 마루미디어는 20년 동안
+                                방송가에 살아온 이들로 이루어진 콘텐츠 제작 업체로, 간단한 촬영에서부터 다양한 영상제작은 물론이고, 사람들이 원하는
+                                홍보영상까지 만들어 드리고 있습니다.{" "}
+                            </Center>
+                        </Card>
+                    </Box>
+                    <Center mt={50} w="100%">
+                        <Image position="absolute" src={"/images/banner_test_5.jpeg"} alt="마루미디어" w="100%" h={500}></Image>
+                        <Box
+                            w="100%"
+                            h={500}
+                            bg={{
+                                linearGradient: {
+                                    colors: ["#00000000", "blue.800"],
+                                    start: [0, 0],
+                                    end: [1, 0],
+                                },
+                            }}
+                        >
+                            www
+                        </Box>
+                    </Center>
                     <ZomatoCategories />
                 </VStack>
 
